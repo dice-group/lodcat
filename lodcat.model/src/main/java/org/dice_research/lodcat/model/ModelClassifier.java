9b1d2d0258db94c060b9c97e4e4afca2eab39de3
Denis Kuchelev <denkv@users.noreply.github.com>
2021-02-10 16:02:29 +0100

    Output document classification results to a CSV file

--- lodcat.model/src/main/java/org/dice_research/lodcat/model/ModelClassifier.java
+++ lodcat.model/src/main/java/org/dice_research/lodcat/model/ModelClassifier.java
@@ -1,7 +1,12 @@
 package org.dice_research.lodcat.model;
 
-import org.dice_research.topicmodeling.utils.doc.DocumentName;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import org.dice_research.topicmodeling.algorithms.ClassificationModel;
 import org.dice_research.topicmodeling.algorithms.ModelingAlgorithm;
 import org.dice_research.topicmodeling.io.CorpusReader;
@@ -11,7 +16,9 @@ import org.dice_research.topicmodeling.io.java.CorpusObjectReader;
 import org.dice_research.topicmodeling.io.ProbTopicModelingAlgorithmStateReader;
 import org.dice_research.topicmodeling.utils.corpus.Corpus;
 import org.dice_research.topicmodeling.utils.doc.Document;
-import org.dice_research.topicmodeling.utils.doc.DocumentClassificationResult;
+import org.dice_research.topicmodeling.utils.doc.DocumentName;
+import org.dice_research.topicmodeling.utils.doc.DocumentURI;
+import org.dice_research.topicmodeling.utils.doc.ProbabilisticClassificationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +30,9 @@ public class ModelClassifier {
     private static final Logger LOGGER = LoggerFactory.getLogger(ModelClassifier.class);
 
     public static void main(String[] args) {
-        if (args.length != 2) {
+        if (args.length != 3) {
             System.err.println(
-                    "Wrong number of arguments! the following format is expected:\n\tModelClassifier <model-file> <corpus-object-file>");
+                    "Wrong number of arguments! the following format is expected:\n\tModelClassifier <model-file> <corpus-object-file> <output-file>");
         }
         File modelFile = new File(args[0]);
         if (!modelFile.getParentFile().exists()) {
@@ -35,9 +42,10 @@ public class ModelClassifier {
         if (!corpusFile.exists()) {
             throw new IllegalArgumentException("The given corpus file does not exist.");
         }
+        File outputFile = new File(args[2]);
 
         ModelClassifier classifier = new ModelClassifier();
-        classifier.run(modelFile, corpusFile);
+        classifier.run(modelFile, corpusFile, outputFile);
     }
 
     private ClassificationModel readClassificationModel(File modelFile) {
@@ -52,18 +60,44 @@ public class ModelClassifier {
         return reader.getCorpus();
     }
 
-    public void run(File modelFile, File corpusFile) {
+    public void run(File modelFile, File corpusFile, File outputFile) {
         LOGGER.trace("Reading model...");
         ClassificationModel model = readClassificationModel(modelFile);
 
         LOGGER.trace("Reading corpus...");
         Corpus corpus = readCorpus(corpusFile);
 
-        for (Document document : corpus) {
-            LOGGER.trace("Document name: {}", document.getProperty(DocumentName.class));
+        try (
+            OutputStream fos = new FileOutputStream(outputFile);
+            Writer osw = new OutputStreamWriter(fos);
+            Writer writer = new BufferedWriter(osw)
+        ) {
+            for (Document document : corpus) {
+                String documentName = document.getProperty(DocumentName.class).getStringValue();
+                LOGGER.trace("Document name: {}", documentName);
 
-            DocumentClassificationResult result = model.getClassificationForDocument(document);
-            LOGGER.trace("Classification result: {}", result);
+                String documentURI = document.getProperty(DocumentURI.class).getStringValue();
+                LOGGER.trace("Document URI: {}", documentURI);
+
+                ProbabilisticClassificationResult result = (ProbabilisticClassificationResult) model.getClassificationForDocument(document);
+                LOGGER.trace("Classification result: {}", result);
+
+                StringBuilder s = new StringBuilder();
+                s.append("\"");
+                s.append(documentName);
+                s.append("\",\"");
+                s.append(documentURI);
+                s.append("\",");
+                s.append(result.getClassId());
+                for (double topicProbability : result.getTopicProbabilities()) {
+                    s.append(",");
+                    s.append(topicProbability);
+                }
+                s.append("\n");
+                writer.write(s.toString());
+            }
+        } catch (IOException e) {
+            LOGGER.error("Exception while writing", e);
         }
     }
 }
